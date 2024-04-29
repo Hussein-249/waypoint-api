96c0a67547923fbd125a30c75a32e89ff5c84f84
Hussein-249 <husseinadi8@gmail.com>
2024-04-29 17:16:35 -0400

    More style edits, map is majority of screen and adding waypoints is smoother.

--- static/JavaScript/loadLeaflet.js
+++ static/JavaScript/loadLeaflet.js
@@ -2,7 +2,7 @@ var additionalWaypoints = 1
 
 function initializeMap() {
     // setting the map in the 'leaflet-map' div
-    var map = L.map('leaflet-map').setView([43.65, -79.35], 8);
+    var map = L.map('leaflet-map').setView([43.65, -79.35], 9);
 
     // tile layer must be specified, else blank map
     L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -13,27 +13,52 @@ function initializeMap() {
 
 
 function dynamicAddWaypointDiv() {
+
     var addWayPointButton = document.getElementById('add-button');
-    var form = document.querySelector('form');
+    var form = document.getElementById('add-waypoint-section');
+    //    var form = document.querySelector('form');
 
     addWayPointButton.addEventListener('click', function() {
+
+    if (additionalWaypoints == 4)
+    {
+        removeTargetInput = document.getElementById('waypoint-nr-' + additionalWaypoints)
+        removeTargetLabel = document.getElementById('waypoint-label-' + additionalWaypoints)
+
+        form.removeChild(removeTargetInput);
+        form.removeChild(removeTargetLabel);
+
+        additionalWaypoints--;
+
+        return;
+    }
+
+
+        if (additionalWaypoints == 3)
+        {
+//            addWayPointButton.style.display = 'none';
+            addWayPointButton.innerHTML = 'Remove Waypoints';
+
+        }
 
         additionalWaypoints++;
 
         var waypointInput = document.createElement('input');
         waypointInput.setAttribute('type', 'text');
-        waypointInput.setAttribute('id', 'waypoint-' + additionalWaypoints);
-        waypointInput.setAttribute('name', 'waypoint-' + additionalWaypoints);
+        waypointInput.setAttribute('id', 'waypoint-nr-' + additionalWaypoints);
+        waypointInput.setAttribute('name', 'waypoint-nr-' + additionalWaypoints);
 
         var waypointLabel = document.createElement('label');
-        waypointLabel.setAttribute('for', 'waypoint-' + additionalWaypoints);
+        waypointLabel.setAttribute('for', 'waypoint-label-' + additionalWaypoints);
         waypointLabel.textContent = 'Point ' + additionalWaypoints;
 
-        form.appendChild(document.createElement('br'));
+        if (additionalWaypoints >= 3)
+        {
+            form.appendChild(document.createElement('br'));
+        }
         form.appendChild(waypointLabel);
         form.appendChild(document.createElement('br'));
         form.appendChild(waypointInput);
-        form.appendChild(document.createElement('br'));
     });
 }
 
